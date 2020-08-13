f1f5d3f1d7a1385068af8f8386c403220a48dfe4
Amira Nazri <anazri@hotmail.com>
2020-08-13 15:13:51 +0200

    draw function is jank

--- red_tetris.js
+++ red_tetris.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('red_tetris');
 const context = canvas.getContext('2d');
 
 context.scale(20, 20)
+context.fillstyle = 'black';
+context.fillRect(0, 0, canvas.width, canvas.height);    
 
 const matrix = [
     [0, 0, 0],
@@ -9,13 +11,11 @@ const matrix = [
     [0, 1, 0],
 ]; 
 
-function draw() {
-    context.fillstyle = 'black';
-    context.fillRect(0, 0, canvas.width, canvas.height);    
-    drawMatrix(player.matrix, player.pos);
-}
+// function draw() {
+//     drawTetromino(player.matrix, player.pos);
+// }
 
-function drawMatrix(matrix, offset) {
+function drawTetromino(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
@@ -26,25 +26,27 @@ function drawMatrix(matrix, offset) {
     });
 }
 
-let dropCounter = 0;
-let dropInterval = 1000; //miliseconds == 1sec
-let previousTime = 0;
+// let dropCounter = 0;
+// let dropInterval = 1000; //miliseconds == 1sec
+// let previousTime = 0;
 
-function update(time = 0) {
-    const deltaTime = time - previousTime;
-    previousTime = time; 
-    // console.log(time);
-    dropCounter += deltaTime;
-    if (dropCounter > dropInterval) {
-        player.pos.y++;
-        dropCounter = 0;
-    }
-    requestAnimationFrame(update);
-} 
+// function update(time = 0) {
+//     const deltaTime = time - previousTime;
+//     previousTime = time; 
+//     // console.log(time);
+//     dropCounter += deltaTime;
+//     if (dropCounter > dropInterval) {
+//         player.pos.y++;
+//         dropCounter = 0;
+//     }
+//     requestAnimationFrame(update);
+// } 
 
-const player = {
-    pos: {x: 5, y: 5},
-    matrix: matrix,
-}
+// const player = {
+//     pos: {x: 5, y: 5},
+//     matrix: matrix,
+// }
 
-update();
+// update();
+
+drawTetromino(matrix, {x:5, y: 5})
